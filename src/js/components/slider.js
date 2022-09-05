a0c22a26c44db44cdbeb0aacdd2e80f230b56715
Ihor Savelyev <savelyev7979@gmail.com>
2022-09-05 23:20:10 +0300

    fix: change slider.js

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,23 +4,26 @@ import { disablePrevNextBtns, listenForPrevBtnClick, listenForNextBtnClick } fro
 import { updateProgressBar } from "./progressBar";
 
 export default function sliderS() {
-  const wrap = document.querySelector(".embla");
-  const viewPort = wrap.querySelector(".embla__viewport");
-  const prevBtn = wrap.querySelector(".embla__button--prev");
-  const nextBtn = wrap.querySelector(".embla__button--next");
-  const progressBar = document.querySelector(".embla__progress__bar");
+  const setupEmblaCarousel = (emblaNode, options) => {
+    const viewPort = emblaNode.querySelector(".embla__viewport");
+    const prevBtn = emblaNode.querySelector(".embla__button--prev");
+    const nextBtn = emblaNode.querySelector(".embla__button--next");
+    const progressBar = document.querySelector(".embla__progress__bar");
 
-  const autoplay = Autoplay({ delay: 10000, stopOnInteraction: false }, (emblaRoot) => emblaRoot.parentElement);
-  const embla = EmblaCarousel(viewPort, { loop: false, slidesToScroll: 1, skipSnaps: false, dragFree: true, containScroll: "trimSnaps" }, [autoplay]);
-  const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
-  const updateProgress = updateProgressBar(progressBar, embla);
+    const autoplay = Autoplay({ delay: 10000, stopOnInteraction: false }, (emblaRoot) => emblaRoot.parentElement);
+    const embla = EmblaCarousel(viewPort, options, [autoplay]);
+    const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
+    const updateProgress = updateProgressBar(progressBar, embla);
 
+    listenForPrevBtnClick(prevBtn, embla, autoplay);
+    listenForNextBtnClick(nextBtn, embla, autoplay);
 
-  listenForPrevBtnClick(prevBtn, embla, autoplay);
-  listenForNextBtnClick(nextBtn, embla, autoplay);
+    embla.on("init", disablePrevAndNextBtns);
+    embla.on("select", disablePrevAndNextBtns);
+    embla.on("scroll", updateProgress);
+  };
 
-  embla.on("init", disablePrevAndNextBtns);
-  embla.on("select", disablePrevAndNextBtns);
-  embla.on("scroll", updateProgress);
-
+  const options = { loop: false, slidesToScroll: 1, skipSnaps: false, dragFree: true, containScroll: "trimSnaps" };
+  const emblaNodes = [].slice.call(document.querySelectorAll(".embla"));
+  const emblaCarousels = emblaNodes.map((emblaNode) => setupEmblaCarousel(emblaNode, options));
 }
