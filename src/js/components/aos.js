1a5186585e61eb950a100acd322c5d9ca50bd263
Clo <savelyev7979@gmail.com>
2023-03-30 00:18:44 +0300

    fix: changed wrapper background-image

--- src/js/components/aos.js
+++ src/js/components/aos.js
@@ -14,8 +14,8 @@ export default function aos(params) {
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 190, // offset (in px) from the original trigger point
-    delay: 250, // values from 0 to 3000, with step 50ms
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 150, // values from 0 to 3000, with step 50ms
     duration: 800, // values from 0 to 3000, with step 50ms
     easing: "ease", // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
